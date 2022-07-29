a03090ea1611b84717c8643931b83c894dbea5c8
pritish-bhatnagar <pritishbhatnagar7b@gmail.com>
2022-07-29 16:51:55 +0530

    Class Program

--- src/class/Program.cs
+++ src/class/Program.cs
@@ -1,83 +1,33 @@
-﻿//By Pritish Bhatnagar
-
-namespace FoodieCorner.ConsoleApp
+﻿namespace pritish
 {
-    
 
-        public class calc
+    public class calc
+    {
+
+        int a, b;
+        
+        public void add(int a, int b)
         {
-
-            int a, b;
-
-            public void add(int a, int b)
-            {
-                int add = a + b;
-                Console.WriteLine("addition is : " + add);
-            }
-            public void sub(int a, int b)
-            {
-
-                int sub = a - b;
-                Console.WriteLine("Subtraction is : " + sub);
-            }
-            public void mul(int a, int b)
-            {
-
-                int mul = a * b;
-                Console.WriteLine("multiplication is : " + mul);
-            }
-            public void div(decimal a, decimal b)
-            {
-
-                decimal div = a / b;
-                Console.WriteLine("division is : " + div);
-            }
-
-
-
-
-
-
-
+            int add = a + b;
+            Console.WriteLine("addition is : " + add);
         }
-
-        public class student
+        public void sub(int a, int b)
         {
-            public string name;
-            public int id, age, clas;
-            public student(string name, int age, int clas, int id)
-            {
-                this.name = name;
-                this.age = age;
-                this.clas = clas;
-                this.id = id;
-                Console.WriteLine("New student has been admitted to the school");
 
-
-            }
-
-
-
-
+            int sub = a - b;
+            Console.WriteLine("Subtraction is : " + sub);
         }
-        public class teacher
+        public void mul(int a, int b)
         {
-            public string name, desig;
-            public int id, age;
-            public teacher(string name, int age, string desig, int id)
-            {
-                this.name = name;
-                this.age = age;
-                this.desig = desig;
-                this.id = id;
-                Console.WriteLine("New object of teacher class has been created!!");
 
+            int mul = a * b;
+            Console.WriteLine("multiplication is : " + mul);
+        }
+        public void div(decimal a, decimal b)
+        {
 
-            }
-
-
-
-
+            decimal div = a / b;
+            Console.WriteLine("division is : " + div);
         }
 
 
@@ -86,18 +36,64 @@ namespace FoodieCorner.ConsoleApp
 
 
 
+    }
 
-
-        public class Program
+    public class student
+    {
+        public string name;
+        public int id, age, clas;
+        public student(string name, int age, int clas, int id)
         {
-            public static void Main(string[] args)
-            {
-                Console.WriteLine("Please enter the class you want to access");
-                Console.WriteLine("\n 1 for accessing Calculator \n 2 for Student \n 3 for Teacher");
-                try
-                {
-                    int sw = Convert.ToInt32(Console.ReadLine());
-                if (sw == 1 || sw == 2 || sw == 3)
+            this.name = name;
+            this.age = age;
+            this.clas = clas;
+            this.id = id;
+            Console.WriteLine("New student has been admitted to the school");
+
+
+        }
+
+
+
+       
+    }
+    public class teacher
+    {
+        public string name, desig;
+        public int id, age;
+        public teacher(string name, int age, string desig, int id)
+        {
+            this.name = name;
+            this.age = age;
+            this.desig = desig;
+            this.id = id;
+            Console.WriteLine("New object of teacher class has been created!!");
+
+
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the class you want to access");
+            Console.WriteLine("\n 1 for accessing Calculator \n 2 for Student \n 3 for Teacher");
+            try { 
+            int sw= Convert.ToInt32(Console.ReadLine());
+                if (sw == 1 || sw == 2 || sw==3)
                 {
                     switch (sw)
                     {
@@ -193,19 +189,6 @@ namespace FoodieCorner.ConsoleApp
 
                             student s1 = new student(na, age, clas, id);
                             Console.WriteLine("His I.D ->" + s1.id);
-                            Console.WriteLine(" wanna see details? press y for yes, n for exit. ");
-                            string see = Console.ReadLine();
-                            if (see == "y")
-                            {
-                                Console.WriteLine(" I.D ->" + s1.id);
-                                Console.WriteLine(" NAME ->" + s1.name);
-                                Console.WriteLine(" AGE ->" + s1.age);
-                                Console.WriteLine(" CLASS ->" + s1.clas);
-                            }
-                            else
-                            {
-                                break;
-                            }
                             break;
 
                         case 3:
@@ -222,43 +205,28 @@ namespace FoodieCorner.ConsoleApp
 
                             teacher t1 = new teacher(name, age1, desig, id1);
                             Console.WriteLine(" I.D ->" + t1.id);
-                            Console.WriteLine(" wanna see details? press y for yes, n for exit. ");
-                            string se = Console.ReadLine();
-                            if (se == "y")
-                            {
-                                Console.WriteLine(" I.D ->" + t1.id);
-                                Console.WriteLine(" NAME ->" + t1.name);
-                                Console.WriteLine(" AGE ->" + t1.age);
-                                Console.WriteLine(" DESIGNATION ->" + t1.desig);
-                            }
-                            else
-                            {
-                                break;
-                            }
                             break;
                     }
                 }
-                else {
-                    Console.WriteLine(" PLEASE ENTER NUMBER BETWEEN 1-3");
-                }
-
-                } catch (Exception ex)
-                {
-                    Console.WriteLine("Please DO NOT ENTER A STRING");
-
-
-                }
-
-
-
-
-
-
-
-
-
+                
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please Enter a number between 1 or 2");
 
 
             }
+
+
+
+
+
+
+
+
+
+
+
         }
     }
+}
